da1d34730b9d558deaa382192aa11466ded558f9
Nazia <nazia.zaman11@gmail.com>
2016-04-04 01:06:51 -0500

    Added files via upload

---
+++ CrossShield.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class CrossShield
+{
+	public static void Main()
+    {
+        int index;
+        int limit = 100;
+        bool flag;
+        for (index = 1; index <= limit; index++)
+        {
+            flag = false;
+            if (index % 2 == 0)
+            {
+                flag = true;
+                Console.Write("Cross");
+            }
+            if (index % 7 == 0)
+            {
+                flag = true;
+                Console.Write("Shield");
+            }
+            if(!flag)
+            {
+                Console.Write(index);
+            }
+            Console.WriteLine();
+        }
+    }
+}
